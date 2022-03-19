5ffee05c692e3f2db7394ffd4114d913124db550
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-03-19 15:50:45 -0400

    ajustando o tema e a tela de cadastro

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.view.mainView import Builder
 
 class Program(MDApp):
     def build(self):
+        self.theme_cls.theme_style = "Dark"
         return Builder.load_file('./src/view/kv/tela_principal.kv')
 
 if __name__ == '__main__':
