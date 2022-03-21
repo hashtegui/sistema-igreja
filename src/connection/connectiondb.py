2a386a51f44fa460d0789c2251dec080a6d469e1
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-03-21 14:05:17 -0400

    implementando o sqlite

--- src/connection/connectiondb.py
+++ src/connection/connectiondb.py
@@ -1,2 +1,24 @@
-class Connection:
-    pass
+from sqlite3 import Error
+import sqlite3
+
+def create_connection(db_file):    
+    con = None
+    
+    try:
+        con = sqlite3.connect('./usuarios.db')  
+    except Error as e:
+        print(e)
+    finally:
+        if con:
+            con.close()
+            
+def prepara_banco():
+    '''
+    CREATE TABLE IF NOT EXISTS pessoas(
+        id integer auto_increment primary key,
+        nome text not null,
+        sobrenome text not null,
+        sexo text not null,
+        dt_nascimento text not null
+    )
+    '''
